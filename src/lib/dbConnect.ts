cfd4b1c3f0388ec4e9a4147d6c85f9383dad09b8
Himanshu88027 <himanshukumar88027@gmail.com>
2024-08-11 10:21:57 +0530

    Databse connection is done

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose, { Connection } from "mongoose";
+
+type ConnectionObject = {
+    isConnected? : Number
+}
+
+const connection: ConnectionObject = {};
+
+async function dbConnect() {
+    // check if we have a database connection if it's currently connecting
+    if (connection.isConnected) {
+        console.log('Already connected to databse');
+        return;
+    }
+
+    //if it's first time, Attempt to connect
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+        connection.isConnected = db.connections[0].readyState;
+        console.log('Databse connected successfuly');
+    } catch (error) {
+        console.error('Failed to connect to database:', error);
+        process.exit(1);
+    }
+}
+
+export default dbConnect;
