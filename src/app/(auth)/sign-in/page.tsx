81eba76447696b42f4fce765f22f420138edba76
Himanshu88027 <himanshukumar88027@gmail.com>
2024-08-21 12:22:18 +0530

    Sign in page complete

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,121 @@
-'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+"use client";
+import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useRouter } from "next/navigation";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { signInSchemas } from "@/schemas/signInSchemas";
+import { signIn } from "next-auth/react";
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+function Signup() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+
+  //zod implementation
+  const form = useForm<z.infer<typeof signInSchemas>>({
+    resolver: zodResolver(signInSchemas),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchemas>) => {
+    setIsSubmitting(true);
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+    if (result?.error) {
+      if (result.error === "CredentialsSignin") {
+        toast({
+          title: "Login Failed",
+          description: "Incorrect username or password",
+          variant: "destructive",
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: result.error,
+          variant: "destructive",
+        });
+      }
+    }
+
+    if (result?.url) {
+      router.replace("/dashboard");
+    }
+
+    setIsSubmitting(false);
+  };
   return (
-    <>
-      Not signed in <br />
-      <button className="px-4 py-2 bg-cyan-400 rounded-lg hover:bg-cyan-500" onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+    <div className="h-screen w-full flex justify-center items-center bg-slate-900 p-4">
+      <div className="sm:w-1/3 w-full h-full rounded-3xl bg-white py-4 px-6 space-y-4">
+        <div className="text-center tracking-tighter space-y-4">
+          <h1 className="text-5xl font-extrabold text-center">
+            Join Pro Feedback
+          </h1>
+          <p className="text-lg tracking-normal">
+            Signup to send anonymous messages
+          </p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isSubmitting} className="w-full">
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </>
+              ) : (
+                "Signup"
+              )}
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 }
+
+export default Signup;
